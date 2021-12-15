b835388dfaaa48c1f089d3814815447034e1bb95
Jihad Mahmoud <jihad.fayz@gmail.com>
2021-12-15 17:08:27 +0400

    Initial Commit

---
+++ FullHunt/FullHunt.h
@@ -0,0 +1,18 @@
+//
+//  FullHunt.h
+//  FullHunt
+//
+//  Created by Jihad Mahmoud on 15/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FullHunt.
+FOUNDATION_EXPORT double FullHuntVersionNumber;
+
+//! Project version string for FullHunt.
+FOUNDATION_EXPORT const unsigned char FullHuntVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullHunt/PublicHeader.h>
+
+
